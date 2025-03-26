6efa6104f271891bbe11e271e9b047165347a4d2
Paul Stern <paulstern45@gmail.com>
2025-03-26 19:37:51 +0300

    draft. add db

---
+++ lib/db.dart
@@ -0,0 +1,37 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:meditation/session.dart';
+
+class DatabaseHelper {
+  static final _databaseName = "sessions.db";
+  static final _databaseVersion = 1;
+
+  // static final DatabaseHelper instance = DatabaseHelper.instance()
+  static Database? _database;
+
+  Future<Database> get db async {
+    _database ??= await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), _databaseName);
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE sessions(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        startedAt INTEGER NOT NULL,
+        endedAt INTEGER NOT NULL,
+        duration INTEGER NOT NULL,
+        streakdays INTEGER,
+    )
+    '''); 
+  }
+}
